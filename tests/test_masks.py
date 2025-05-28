394ffaba656c2f3ecdf1e10cdd3a989264323919
Your Name <you@example.com>
2025-05-28 14:02:53 +0300

    Все проверено

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,28 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_card_number():
-    assert get_mask_card_number('2202345612340099') == '2202 34** **** 0099'
+@pytest.mark.parametrize(
+    "data, result",
+    [
+        ("1596837868705199", "1596 83** **** 5199"),
+        ("64686473678894779589", "Введен неверный номер карты"),
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("35383033474447895560", "Введен неверный номер карты"),
+        ("6831982476737658", "6831 98** **** 7658"),
+        ("8990922113665229", "8990 92** **** 5229"),
+        ("5999414228426353", "5999 41** **** 6353"),
+        ("73654108430135874305", "Введен неверный номер карты"),
+    ],
+)
+def test_get_mask_card_number(data, result):
+    assert get_mask_card_number(data) == result
 
-def test_get_mask_account():
-    assert get_mask_account('22023456123400991234') == '**1234'
+
+@pytest.mark.parametrize(
+    "data, result",
+    [("64686473678894779589", "**9589"), ("35383033474447895560", "**5560"), ("73654108430135874305", "**4305")],
+)
+def test_get_mask_account(data, result):
+    assert get_mask_account(data) == result
